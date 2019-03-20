cd06c0693bf59b1e5db1ecbfe2b8356b210db3ce
Kirill <8338050@gmail.com>
2019-03-20 20:11:01 +0300

    wip

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const MongoClient = require('mongodb').MongoClient;
 
 
 const app = express()
@@ -12,26 +13,39 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/statistic', (req, res) => {
-  data.find({}, function(err, data) { console.log(err, data, data.length); });
-  if (error) { console.error(error); }
-  data.find({}, (data, error) => {
-    console.log(data);
+app.get('/statistic', (req, res) => { 
+  db.collection('statistic').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
     res.send({
-      data: data
-    })
-  })
+    data: data
+     }) 
+  });
+  // Stat.find({}, (data, err) => {
+  //   console.log(data);
+  //   res.send({
+  //     data: data
+  //   }) 
+  // })
 })
 
-let data = {};
+/*const model = new Schema({ timestamp: Number, totalCount: Number, fetchedCount: Number });
+const Stat = mongoose.model("statistic", model);
+
 mongoose.connect('mongodb://localhost:27017/crawler');
-mongoose.model('statistic', new Schema({ timestamp: Number, totalCount: Number, fetchedCount: Number }));
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
-  data = mongoose.model('statistic');
+});*/
+var database = {};
+MongoClient.connect('mongodb://localhost:27017/crawler', function(err, dbConnection) {
+  if (err) {
+    throw err;
+  }
+  database = dbConnection.db();
 });
 
 app.listen(process.env.PORT || 8081)
